ed3e2c81a3762c5ca330f4730509a206e725b53c
adebisititi <adebisiwaliyat750@gmail.com>
2022-06-21 11:41:14 -0700

    Add the omitted semi-colon

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 let name= "My name is Adebisi Waliyat.";
 alert(name);
-let height= "My height is 1.70M"
+let height= "My height is 1.70M";
 alert(height);
-let country= "My country is Nigeria."
+let country= "My country is Nigeria.";
 alert(country);
